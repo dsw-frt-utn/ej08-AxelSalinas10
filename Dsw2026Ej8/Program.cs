842aee61836e9a6069937a7e134e119a8cec6b87
AxelSalinas10 <Axel.Salinas@alu.frt.utn.edu.ar>
2026-05-03 20:26:05 -0300

    Problema 2 resuelto

--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -1,3 +1,5 @@
 ﻿using Dsw2026Ej8.Problema1;
-
+using Dsw2026Ej8.Problema2;
 Console.WriteLine(ProductHelper.ObtenerEtiquetaProducto(123, "Teclado", 1500));
+var p2 = new Problema2();
+Console.WriteLine(p2.CrearResumenVenta(1, "Mouse", 2, 100));
